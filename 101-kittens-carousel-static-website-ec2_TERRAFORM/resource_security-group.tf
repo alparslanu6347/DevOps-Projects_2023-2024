9dc50ba011c336908a6db8cdeee1a7795939ae94
alparslanu6347 <alparslanu6347@gmail.com>
2024-03-28 23:56:04 +0300

    resource_security-group.tf added

---
+++ 101-kittens-carousel-static-website-ec2_TERRAFORM/resource_security-group.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "arrow_sec" {
+  name        = "arrow-secgrp"
+  description = "arrow-secgrp-SSH-HTTP"
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "arrow-web-secgrp"
+  }
+}
